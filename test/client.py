600dbd0ee189d67e4217fb17dfeb4000e2ce508e
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2024-04-19 18:53:15 +0200

    extend client test code

--- test/client.py
+++ test/client.py
@@ -2,7 +2,10 @@ import inspect
 import RPyCRobotRemote
 
 conn = RPyCRobotRemote.Client()
-print(conn.get_answer)
+print(conn.get_answer())
+print(conn.dummy_test())
+print(conn.get_region())
+print(list(conn.get_region()))
 print(conn.ROBOT_LIBRARY_DOC_FORMAT)
 print(conn.__doc__)
 print(conn.get_answer())
